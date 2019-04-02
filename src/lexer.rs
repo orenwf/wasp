82bae00a38114afb0c7835268f266ea99d2f8d6b
orenwf <oren.friedman07@myhunter.cuny.edu>
2019-04-02 19:07:30 -0400

    lexingl

--- src/lexer.rs
+++ src/lexer.rs
@@ -54,6 +54,7 @@ pub enum Token {
     Space,
     Dot,
     Comment,
+    DQuote,
         // Mappings
     Plus,
     Minus,
@@ -66,53 +67,76 @@ pub enum Token {
     Find, // for tables
 }
 
-impl Token {
-    fn match_tok(lexeme: str) -> Result<(Option<Token>,Option<char>)> {
-        match lexeme {
-             // special form
-            "def" => Define, // Bind symbol to value
-            "fn" => Function, // Special Form: Instantiates or references callable code
-            "tab" => Table, // Instantiates or references an aggregate data type
-            "case" => Case, // for control flow
-            "yield" => Yield, // for control flow
-            "let" => Let,
-            "quote" => Quote,
-            // Identifier
-            Identifier(str),
-            // Literals
-            Decimal(Number),
-            String_Lit(str),
-            // Builtins
-                // Predicate
-            Equals,
-            Not,
-            LT,
-            GT,
-            LEQ,
-            GEQ,
-            And,
-            Or,
-            Is,
-            In, // for tables
-                // Separators
-            LParen,
-            RParen,
-            LBrace,
-            RBrace,
-            WSpace,
-            Dot,
-            Comment,
-                // Mappings
-            Plus,
-            Minus,
-            Mul,
-            Div,
-            Mod,
-            Exp,
-            First,
-            Rest,
-            Find, // for tables          
-        }
+enum Symbol {
+    Matched(Token),
+    Unmatched(char),
+}
+
+enum Lexeme {
+    Matched(Token),
+    Unmatched(str),
+}
+
+fn match_delim(lexeme: char) - Option<Symbol> {
+    match lexeme {
+        ' ' => None,
+        '\n' => None,
+        '(' => Symbol::Matched(LParen),
+        ')' => Symbol::Matched(RParen),
+        '[' => Symbol::Matched(LBrace),
+        ']' => Symbol::Matched(RBrace),
+        ';' => Symbol::Matched(Comment),
+        '.' => Symbol::Matched(Dot),
+        '\"' => Symbol::Matched(DQuote),
+        _ => Symbol::Unmatched(_),
+    }
+}
+
+fn match_tok(lexeme: str) -> Lexeme {
+    match lexeme {
+         // special form
+        "def" => Lexeme::Matched(Define), // Bind symbol to value
+        "fn" => Lexeme::Matched(Function), // Special Form: Instantiates or references callable code
+        "tab" => Lexeme::Matched(Table), // Instantiates or references an aggregate data type
+        "case" => Lexeme::Matched(Case), // for control flow
+        "yield" => Lexeme::Matched(Yield), // for control flow
+        "let" => Lexeme::Matched(Let),
+        "quote" => Lexeme::Matched(Quote),
+        // Builtins
+            // Predicate
+        "=" => Lexeme::Matched(Equals),
+        "not" => Lexeme::Matched(Not),
+        "<" => Lexeme::Matched(LT),
+        ">" => Lexeme::Matched(GT),
+        "<=" => Lexeme::Matched(LEQ),
+        ">=" => Lexeme::Matched(GEQ),
+        "and" => Lexeme::Matched(And),
+        "or" => Lexeme::Matched(Or),
+        "is" => Lexeme::Matched(Is),
+        "in" => Lexeme::Matched(In), // for tables
+            // Mappings
+        "+" => Lexeme::Matched(Plus),
+        "-" => Lexeme::Matched(Minus),
+        "*" => Lexeme::Matched(Mul),
+        "/" => Lexeme::Matched(Div),
+        "%" => Lexeme::Matched(Mod),
+        "^" => Lexeme::Matched(Exp),
+        "first" => Lexeme::Matched(First),
+        "rest" => Lexeme::Matched(Rest),
+        "find" => Lexeme::Matched(Find), // for tables
+        _ => Lexeme::Unmatched(_),
+    }
+}
+
+fn match_lit(lexeme: str) -> Result<Lexeme> {
+    match lexeme
+}
+
+fn fail(msg: str) {
+    panic!(
+        "The following code did not match any syntax: {:?}",
+        msg
+        )
 }
 
 pub fn lex(input: String) -> Vec<Token> {
@@ -122,27 +146,26 @@ pub fn lex(input: String) -> Vec<Token> {
     loop {
         current = cursor.next();
         if current == None {
+            match match_tok(lexeme) {
+                Lexeme::Matched(tok) => tokens.push(tok),
+                Lexeme::Unmatched(_) => fail(_),
+            }
             break;
         }
-        lexeme.push(current);
-        let res = match tokenize(lexeme) {
-            Ok(leftover, token) => match tup {
-                Some(token) => {
-                    tokens.push(token);
-                    Ok()
-                },
-                None => None
+        match match_delim(current) {
+            Some(symbol) => match symbol {
+                Symbol::Matched(tok) => tokens.push(tok),
+                Symbol::Unmatched(_) => lexeme.push(_),
             },
-            Err(msg) => {
-                panic!(
-                    "The following code did not match any syntax: {:?}",
-                    msg
-                )
-            },
+            None => (), // likely whitespace
         }
-        if res == Ok() {
-            lexeme.clear()
+        match match_tok(lexeme) {
+            Lexeme::Matched(tok) => tokens.push(tok),
+            Lexeme::Unmatched(_) => 
+                match match_lit(_) {
+                    Ok(tok) => tokens.push(tok),
+                    Err(_) => fail(_),
+                },
         }
     }
-    tokens
 }
